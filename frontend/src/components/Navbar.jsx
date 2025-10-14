c1fa4456b3eb132eb704e189d1d611485960a3ea
Petter <petterah@outlook.com>
2025-10-14 17:58:04 +0200

    phpne

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,14 +5,14 @@ import Logo from "../../assets/logo";
 const Navbar = () => {
     return (
       <nav className = "bg-white shadow-md">
-        <div className= "w-full px-50 flex justify-between items-center h-16">
+        <div className= "w-full px-4 sm:px-6  lg:px-10 flex justify-between items-center h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
             <Logo />
             <span className="font-bold text-xl text-gray-800">PilsRadar</span>
           </Link>
           {/* Meny */}
-          <ul className = "flex space-x-6">
+          <ul className = "flex space-x-3 sm:space-x-6 text-sm sm:text-base">
             <li>
               <Link to="/"
               className="px-3 py-2 rounded-md text-gray-700 hover:text-blue-500 hover:bg-gray-100 transition"
